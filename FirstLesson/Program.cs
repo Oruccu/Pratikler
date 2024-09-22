ceb8035b1b006469ba17fe5cd6fc784b9c069ead
Ayse Oruccu <oruccu.ayse1999@gmail.com>
2024-09-22 12:58:58 +0300

    First Commit

---
+++ FirstLesson/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Adınızı giriniz: "); //The user was asked to enter his/her name
+string userName = Console.ReadLine();
+
+Console.WriteLine("Soyadınızı giriniz"); //The user was asked to enter his/her username
+string  surname = Console.ReadLine();
+
+Console.WriteLine("Patika Programına olma nedeninizi yazınız"); //The user was asked what they expected from the Patika
+string expectation = Console.ReadLine();
+
+Console.WriteLine("Hoşgeldin: " + userName + surname);
+Console.WriteLine("Patika sana " + expectation + "sağlayacak");
+
+
